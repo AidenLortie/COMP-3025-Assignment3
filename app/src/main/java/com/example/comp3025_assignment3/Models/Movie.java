b3058aeda96cb195eeb507816d914d3b4c28c335
David <156011127+DavidSimmons05@users.noreply.github.com>
2025-04-06 15:25:31 -0400

    thing

--- app/src/main/java/com/example/comp3025_assignment3/Models/Movie.java
+++ app/src/main/java/com/example/comp3025_assignment3/Models/Movie.java
@@ -1,4 +1,53 @@
 package com.example.comp3025_assignment3.Models;
 
 public class Movie {
+    public String title, studio, poster, rating, description;
+
+    public Movie(String title, String studio, String poster, String rating, String description) {
+        this.title = title;
+        this.studio = studio;
+        this.poster = poster;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
