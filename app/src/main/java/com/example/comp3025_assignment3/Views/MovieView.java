54ce2ebd9ad65ce99c0c5b2e17d8a8d54386a3fe
David <156011127+DavidSimmons05@users.noreply.github.com>
2025-04-06 17:37:27 -0400

    working on stuff

--- app/src/main/java/com/example/comp3025_assignment3/Views/MovieView.java
+++ app/src/main/java/com/example/comp3025_assignment3/Views/MovieView.java
@@ -1,4 +1,30 @@
 package com.example.comp3025_assignment3.Views;
 
-public class MovieView {
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.comp3025_assignment3.Models.Movie;
+import com.example.comp3025_assignment3.ViewModels.MovieSearchViewModel;
+import com.example.comp3025_assignment3.ViewModels.MovieViewModel;
+import com.example.comp3025_assignment3.databinding.SearchDetailsBinding;
+
+public class MovieView extends AppCompatActivity {
+    Movie movie;
+    String movieId;
+    SearchDetailsBinding binding;
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        binding = SearchDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intObj = getIntent();
+        movieId = intObj.getStringExtra("MOVIE_ID");
+
+        //MovieViewModel viewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+    }
 }
