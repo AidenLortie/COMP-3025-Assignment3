69cbca579da6d529439db9020a23d386037df937
AidenLortie <146017618+AidenLortie@users.noreply.github.com>
2025-04-06 15:36:36 -0400

    Added Utils to APIUtil

--- app/src/main/java/com/example/comp3025_assignment3/Utils/APIUtil.java
+++ app/src/main/java/com/example/comp3025_assignment3/Utils/APIUtil.java
@@ -1,4 +1,32 @@
 package com.example.comp3025_assignment3.Utils;
 
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class APIUtil {
+
+    private static final OkHttpClient client = new OkHttpClient();
+
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    private static final String baseUrl = "https://omdbapi.com/?apikey=a3dbd1e7&type=movie";
+
+    // getMovies - used durring search to get list of movies
+    public static void getMovies(String search, Callback callback) {
+        Request request = new Request.Builder()
+                .url(baseUrl + "&s=" + search)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+    // getMovie - used to get a singular movie
+    public static void getMovie(String movieId, Callback callback){
+        Request request = new Request.Builder()
+                .url(baseUrl + "&i=" + movieId)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
 }
